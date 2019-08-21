68c38cdfd3c83f5cc4bacd157d7a44739c0ede5a
Mr.Phobia <tossarporn@gmail.com>
2019-08-21 16:52:10 +0700

    change

--- service/get_data_rent.php
+++ service/get_data_rent.php
@@ -40,25 +40,25 @@ $detail = [];
 //     }
 // }
 
-//     if(isset($_POST['tec_id'])){
-//          $tec_id = $_POST['tec_id'];
-// $select = "SELECT * FROM `customerforrent` WHERE `ref_id_tec`='".$_POST['tec_id']."' ORDER BY `date_service` DESC";
-//         if($res = mysqli_query($connection,$select)){
-//                 // $row = mysqli_fetch_assoc($res);
-//                 // $detail[] = $row;
-//             while($row = mysqli_fetch_assoc($res)){
-//                 // $row['status_guest'] = status_rent($row['ref_regis'],$connection);
-//                 $detail[] = $row;
-//             }
-//         }
-//         else{
-//           $detail['message']  = "ไม่สามารถติดต่อข้อมูลได้";
-//         }
-//     }
-//     else {
-//         $detail['message']  = "ไม่สามารถติดต่อข้อมูลได้";
-//     }
-// echo json_encode($detail);
+    if(isset($_POST['tec_id'])){
+         $tec_id = $_POST['tec_id'];
+$select = "SELECT * FROM `customerforrent` WHERE `ref_id_tec`='".$_POST['tec_id']."' ORDER BY `date_service` DESC";
+        if($res = mysqli_query($connection,$select)){
+                // $row = mysqli_fetch_assoc($res);
+                // $detail[] = $row;
+            while($row = mysqli_fetch_assoc($res)){
+                // $row['status_guest'] = status_rent($row['ref_regis'],$connection);
+                $detail[] = $row;
+            }
+        }
+        else{
+          $detail['message']  = "ไม่สามารถติดต่อข้อมูลได้";
+        }
+    }
+    else {
+        $detail['message']  = "ไม่สามารถติดต่อข้อมูลได้";
+    }
+echo json_encode($detail);
     // function status_rent ($ref_id_guest,$connection){
     //     $select_ref = "SELECT `check_tus` FROM `guest_register` WHERE `id`='{$ref_id_guest}'";
        
