9e78a78d9b93c0e118ae305819cf64cca42d87d4
Mr.Phobia <tossarporn@gmail.com>
2019-08-21 16:33:21 +0700

    status

--- service/get_data_rent.php
+++ service/get_data_rent.php
@@ -60,8 +60,8 @@ $select = "SELECT * FROM `customerforrent` WHERE `ref_id_tec`='".$_POST['tec_id'
     }
 echo json_encode($detail);
     function status_rent ($ref_id_guest,$connection){
-        $select = "SELECT guest_register.check_tus FROM `guest_register` WHERE `id`='{$ref_id_guest}'";
-        if($res = mysqli_query($connection,$select_ref)){
+        $select = "SELECT `check_tus` FROM `guest_register` WHERE `id`='{$ref_id_guest}'";
+        if($res = mysqli_query($connection,$select)){
             while($row = mysqli_fetch_assoc($res)){         
                  $row_s[] = $row;
             }
